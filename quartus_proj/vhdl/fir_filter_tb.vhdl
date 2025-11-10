a4acddfbe903d87a885e886d117ed14b7e3fc397
thomas <info@tasoft.ch>
2025-11-10 18:12:19 +0100

    Umsetzung Filter zum Zweiten

--- quartus_proj/vhdl/fir_filter_tb.vhdl
+++ quartus_proj/vhdl/fir_filter_tb.vhdl
@@ -55,7 +55,88 @@ end process;
 
 p_tests: process
 begin
-    wait for 1 us;
+    Av_S_AUDIO_IN_valid <= '0';
+    Av_S_AUDIO_IN <= "0000000000000000";
+
+    FIR_Coeffs <= (
+        x"0013", x"0008", x"0005", x"000C",
+        x"0016", x"0025", x"0037", x"004E",
+        x"0069", x"008B", x"00B2", x"00E0",
+        x"0114", x"014E", x"018E", x"01D3",
+        x"021D", x"026A", x"02BA", x"030B",
+        x"035B", x"03AA", x"03F5", x"043B",
+        x"047B", x"04B2", x"04E0", x"0504",
+        x"051C", x"0528", x"0528", x"051C",
+        x"0504", x"04E0", x"04B2", x"047B",
+        x"043B", x"03F5", x"03AA", x"035B",
+        x"030B", x"02BA", x"026A", x"021D",
+        x"01D3", x"018E", x"014E", x"0114",
+        x"00E0", x"00B2", x"008B", x"0069",
+        x"004E", x"0037", x"0025", x"0016",
+        x"000C", x"0005", x"0001", x"0000",
+        x"0000", x"0000", x"0000", x"0000"
+    );
+
+    wait until Av_S_AUDIO_IN_ready = '1' for 100 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0111111111111111";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    Av_S_AUDIO_IN_valid <= '1';
+    Av_S_AUDIO_IN <= "0000000000000000";
+    wait for 50 ns;
+    Av_S_AUDIO_IN_valid <= '0';
+    wait for 700 ns;
+    Av_S_AUDIO_OUT_ready <= '1';
+    wait until Av_S_AUDIO_OUT_valid = '1' for 700 ns;
+
+    wait for 300 ns;
+
     enabled <= false;
     wait;
 end process;
